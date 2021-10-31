1ffba4bc2bdf3f255f9fc3939da7678aeaca6add
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2021-10-31 08:31:53 +0200

    AverageIndexSumsOfArray

---
+++ AverageIndexSumsOfArrayClassLibrary/AverageIndexSumsOfArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AverageIndexSumsOfArrayClassLibrary
+{
+    public class AverageIndexSumsOfArray
+    {
+        public int GetIndex(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (array.Length < 3) throw new ArgumentException("array.Length < 3");
+
+            var left = array[0];
+            var right = SumRightElements(array);
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (left == right)
+                    return i;
+
+                left += array[i];
+                right -= array[i + 1];
+            }
+
+            return -1;
+        }
+
+        private int SumRightElements(int[] array)
+        {
+            int sum = 0;
+            for (int i = 2; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AverageIndexSumsOfArrayProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AverageIndexSumsOfArrayClassLibrary;
+using static System.Console;
+
+namespace AverageIndexSumsOfArrayProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example1 = new int[] {1, 2, 3, 4, 5,};
+            var example2 = new int[] {1, 0, 2, 20, 3};
+
+            var averageIndex = new AverageIndexSumsOfArray();
+
+            ShowConsoleResults(averageIndex, example1, example2);
+            ReadKey();
+        }
+
+        private static void FormatedWriteLine(int[] array, int average)
+        {
+            WriteLine($"Array: {{ {string.Join(", ", array)} }} | Average: {average.ToString()}");
+        }
+
+        private static void ShowConsoleResults(AverageIndexSumsOfArray averageIndex, params int[][] arrays)
+        {
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                FormatedWriteLine(arrays[i], averageIndex.GetIndex(arrays[i]));
+            }
+        }
+    }
+}
